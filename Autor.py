cfabba8a5fc8caf1c21dda0ac805ddfbcc88d8c4
Cintiabge <cintiabcge@gmail.com>
2024-02-04 21:52:06 -0300

    wip - criando métodos da classe autor

--- Autor.py
+++ Autor.py
@@ -1,5 +1,13 @@
-from Pessoa import *
+class Autor:
+    def _init_(self):
+        self._id_autor = 0
+        self._nome_autor = ''
 
-class Autor(Pessoa):
-    def __init__(self, nome, telefone, nacionalidade):
-        super().__init__(nome, telefone, nacionalidade)
+    # método para adicionar autor
+    def adicionar_autor(self, _id_autor, _nome_autor):
+        self._id_autor = _id_autor
+        self._nome_autor = _nome_autor
+
+autor = Autor()
+autor.adicionar_autor(4, 'alberto')
+print(autor._id_autor, autor._nome_autor)
