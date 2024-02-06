50604792153fb665da20b57d927793348d404658
Cintiabge <cintiabcge@gmail.com>
2024-02-05 21:39:26 -0300

    wip - Método adicionar autor funcionando

--- autor.py
+++ autor.py
@@ -1,13 +1,52 @@
+import random
 class Autor:
-    def _init_(self):
-        self._id_autor = 0
+    def __init__(self):
+        self._id_autor = random.randint(0, 100)
         self._nome_autor = ''
 
     # método para adicionar autor
-    def adicionar_autor(self, _id_autor, _nome_autor):
+    def adicionar_autor(self, _id_autor):
         self._id_autor = _id_autor
-        self._nome_autor = _nome_autor
+        self._nome_autor = input('Digite o nome do Autor: ')
+
+
+    # método para editar autor
+    def editar_autor(self, novo_nome):
+        self._nome_autor = novo_nome 
+
+    # método para mostrar autor
+    def mostrar_autor(self):
+        print(f"ID do Autor: {self._id_autor}")
+        print(f"Nome do Autor: {self._nome_autor}")
+    
+    # método para deletar autor
+    def deletar_autor(self):
+        self._id_autor = 0
+        self._nome_autor = ''
+
+    @property
+    def nome_autor(self):
+        return self._nome_autor
+
+    @nome_autor.setter
+    def nome_autor(self, value):
+        self._nome_autor = str(value)
+
+
 
 autor = Autor()
-autor.adicionar_autor(4, 'alberto')
+# autor.mostrar_autor()
+# autor.adicionar_autor(random)
+# print(usuario.id_usuario)
+
+# autor.mostrar_autor()
+#print(autor._nome_autor)
+
+# autor.editar_autor('Novo')
+# autor.mostrar_autor()
 print(autor._id_autor, autor._nome_autor)
+
+# autor.deletar_autor()
+# autor.mostrar_autor()
+# print(autor._id_autor,autor._nome_autor)
+
